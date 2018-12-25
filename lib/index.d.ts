4a413dbf411398d9147976c12c4da4dde0cecd61
ycjcl868 <45808948@qq.com>
2018-12-25 19:12:51 +0800

    fix: .gitignore lib dir

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { IOptions, ITransformOptions } from './typings';
-declare class JSX2CSS {
-    private type;
-    constructor(options?: IOptions);
-    transform(options: ITransformOptions): any;
-}
-export default JSX2CSS;
