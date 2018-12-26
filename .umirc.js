5087936712a88ee2305e8ed2f836dbe27dafd331
ycjcl868 <45808948@qq.com>
2018-12-26 22:52:50 +0800

    fix: site publish

--- .umirc.js
+++ .umirc.js
@@ -1,8 +1,8 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 export default {
-  base: '/jsx2css',
-  publicPath: '/jsx2css',
+  base: '/jsx2css/',
+  publicPath: '/jsx2css/',
   outputPath: './site',
   chainWebpack(config, { webpack }) {
     config.plugin('moncaco').use(new MonacoWebpackPlugin());
